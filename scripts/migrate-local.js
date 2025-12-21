34d27a89586185cbff286d210792b109eb6f6ddc
palika55555 <pavoldomonkos38@gmail.com>
2025-12-21 16:33:24 +0100

    sa

--- scripts/migrate-local.js
+++
@@ -1,8 +0,0 @@
-import { getLocalDb } from '../config/database.js';
-import { createLocalSchema } from '../models/database-schema.js';
-
-const db = getLocalDb();
-createLocalSchema(db);
-console.log('Local database schema created/updated successfully');
-db.close();
-
